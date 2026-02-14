61f8145db1e0f872ad0b017c5c6ac9615f02d64a
Ahmed Hassop <Ahmed Hassob>
2026-02-14 12:09:17 +0200

    add: github pages package

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,18 @@ import "./i18n";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
-  <Router>
+  <>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>,
       <ReactQueryDevtools buttonPosition="bottom-left" />
     </QueryClientProvider>
     <Toaster
@@ -24,6 +26,6 @@ createRoot(document.getElementById('root')!).render(
         },
       }}
     />
-  </Router>,
+  </>
 
 )
