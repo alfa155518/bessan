e2418c81e4cf799ef40abe164dbaa96b5d7c4c92
Ahmed Hassop <112353313+alfa155518@users.noreply.github.com>
2026-02-14 12:04:41 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/news" element={<NewsAndEvents />} />
