744a6729923a9efe60e9ca23377aa52eadc0b5ea
Ahmed Hassop <Ahmed Hassob>
2026-02-14 11:55:36 +0200

    add: github pages package

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/bessan-frontend/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/news" element={<NewsAndEvents />} />
